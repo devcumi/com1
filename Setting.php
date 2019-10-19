7817ef9f0d3564de4c323d648aac2e58acd9187d
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-19 20:53:48 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,4 +1,3 @@
 <?php
 $bot_bitcoinfauchet="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvYUR5Y+%U5MHgiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvR+%dHVXVYbE+%MYjdLM+%FjM1QifQFV8FV8";
-
 ?>
