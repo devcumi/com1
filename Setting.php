1ccdc31a7bf2f7ebfdc7c2513e217dd8c109c152
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-20 01:35:05 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -15,5 +15,5 @@ Support : Member Family Vlll    Grup Tele : @jancokkau990
 
 
 $bot_bitcoinfauchet="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvYUR5Y+%U5MHgiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvR+%dHVXVYbE+%MYjdLM+%FjM1QifQFV8FV8";
-$hapus="on";
+$hapus="of";
 ?>
