7444349505947b39e5ccecd89a7fd3502c935cf3
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-20 01:17:14 +0700

    Update Setting.php

--- Setting.php
+++ Setting.php
@@ -1,3 +1,18 @@
 <?php
+$btc_fauchet= "\033[1;36m●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●\n
+\033[0;31m
+████████╗███████╗ █████╗ ███╗   ███╗     █████╗ 
+╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ██╔══██╗
+\033[1;33m   ██║   █████╗  ███████║██╔████╔██║    ╚█████╔╝
+   ██║   ██╔══╝  ██╔══██║██║╚██╔╝██║    ██╔══██╗
+\033[1;32m   ██║   ███████╗██║  ██║██║ ╚═╝ ██║    ╚█████╔╝
+   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚════╝ 
+                                                                                                                                                                                                                            
+\033[1;36m●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━
+\033[1;33mCreator : Tim Ninja             Chanel YT : Family Vlll
+Support : Member Family Vlll    Grup Tele : @jancokkau990
+\033[1;36m●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━●━━━━\n";
+
+
 $bot_bitcoinfauchet="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvYUR5Y+%U5MHgiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvR+%dHVXVYbE+%MYjdLM+%FjM1QifQFV8FV8";
 ?>
