0b184fc36fa442a35efd7f32ddf8dc5d6e148c16
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-19 22:47:20 +0700

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -38,16 +38,5 @@ function token($data){
   return $result;
 }
 
-function curl($url,$headers,$data){
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-  $result = curl_exec($ch);
-  curl_close($ch);
-  return $result;
-}
+
+?>
