ed602374e8637f4e67c744c4d325cea7cf8e2f00
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-20 01:04:06 +0700

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -23,20 +23,4 @@ function get_info($mode){
 }
 
 
-function token($data){
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, 'https://securetoken.googleapis.com/v1/token?key=AIzaSyCo3hN1-PCzmhtgT0iGOn_u0sKQzhlS-Qg');
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  $headers = array();
-  $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, 'grant_type=refresh_token&refresh_token='.$data);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-  $result = curl_exec($ch);
-  curl_close ($ch);
-  return $result;
-}
-
-
 ?>
